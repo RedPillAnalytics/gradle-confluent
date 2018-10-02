1f47578961a2ce636be16a715c2d61048a013293
Stewart Bryson <stewart@redpillanalytics.com>
2018-10-01 23:35:37 -0400

    Removing old build and provisioning scripts.

--- scripts/datagen-init.sh
+++
@@ -1,5 +0,0 @@
-ksql-datagen -daemon quickstart=clickstream format=avro topic=clickstream maxInterval=100 iterations=500000
-
-ksql-datagen quickstart=clickstream_codes format=avro topic=clickstream_codes maxInterval=20 iterations=100
-
-ksql-datagen quickstart=clickstream_users format=avro topic=clickstream_users maxInterval=10 iterations=1000
