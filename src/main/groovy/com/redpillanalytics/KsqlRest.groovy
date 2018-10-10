6d4b002a427224c1ae458240ad8ebc8372bdb0cb
Stewart Bryson <stewart@redpillanalytics.com>
2018-10-10 04:38:29 -0400

    Working sample using Unirest with hard-coded statements. Just need to figure out how to use objects/types.

--- src/main/groovy/com/redpillanalytics/KsqlRest.groovy
+++ src/main/groovy/com/redpillanalytics/KsqlRest.groovy
@@ -1,22 +1,9 @@
 package com.redpillanalytics
 
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.Unirest
 import groovy.util.logging.Slf4j
-//import groovyx.net.http.NativeHandlers
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.Response
-
-//import wslite.rest.ContentType
-//import wslite.rest.RESTClient
-//import groovyx.net.http.RESTClient
-//import static groovyx.net.http.ContentType.*
-
-//import static groovyx.net.http.HttpBuilder.configure
-//import static groovyx.net.http.ContentTypes.*
 
 @Slf4j
 class KsqlRest {
@@ -29,7 +16,7 @@ class KsqlRest {
    /**
     * GSON object for parsing objects to JSON.
     */
-   Gson gson = new GsonBuilder().setPrettyPrinting().create()
+   Gson gson = new Gson()
 
    /**
     * Executes a KSQL statement using the KSQL RESTful API.
@@ -44,25 +31,14 @@ class KsqlRest {
 
       def prepared = (sql + ';').replace('\n', '').replace(';;', ';')
 
-      String json = gson.toJson([ksql: prepared, streamsProperties: properties])
+      HttpResponse<String> response = Unirest.post("http://localhost:8088/ksql")
+              .header("Content-Type", "application/vnd.ksql.v1+json")
+              .header("Cache-Control", "no-cache")
+              .header("Postman-Token", "473fbb05-9da1-4020-95c0-f2c60fed8289")
+              .body("{  \"ksql\":\"CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varchar, status int, userid int, bytes bigint, agent varchar) with (kafka_topic = 'clickstream', value_format = 'json');\",\"streamsProperties\":{\"ksql.streams.auto.offset.reset\": \"earliest\"}}")
+              .asString()
 
-      log.warn "json: ${json}"
-
-      log.warn "Executing statement: $prepared"
-
-      OkHttpClient client = new OkHttpClient()
-
-      MediaType mediaType = MediaType.parse("application/json");
-      RequestBody body = RequestBody.create(mediaType, $/{"ksql": "LIST PROPERTIES;",  "streamsProperties": { }/$)
-      Request request = new Request.Builder()
-              .url("${baseUrl}/ksql")
-              .post(body)
-              .addHeader("Content-Type", "application/json")
-              .build()
-
-      Response response = client.newCall(request).execute()
-
-      log.warn "response: ${response.toString()}"
+      log.warn "response: ${response.dump()}"
    }
 
    /**
