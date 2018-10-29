3fae6c85cec21eab228b12ad598a45ac7c3ae6db
Stewart Bryson <stewart@redpillanalytics.com>
2018-10-29 13:50:25 -0400

    Added default values to ConfluentPluginExtension.

--- src/main/groovy/com/redpillanalytics/gradle/ConfluentPluginExtension.groovy
+++ src/main/groovy/com/redpillanalytics/gradle/ConfluentPluginExtension.groovy
@@ -6,87 +6,87 @@ import groovy.util.logging.Slf4j
 class ConfluentPluginExtension {
 
    /**
-    * The group name to use for all tasks.
+    * The group name to use for all tasks. Default: 'confluent'.
     */
    String taskGroup = 'confluent'
 
    /**
-    * Enable KSQL pipeline support.
+    * Enable KSQL pipeline support. Default: true.
     */
    Boolean enablePipelines = true
 
    /**
-    * Enable KSQL UD(A)F support.
+    * Enable KSQL UD(A)F support. Default: true.
     */
    Boolean enableFunctions = true
 
    /**
-    * Enable Kafka Streams support.
+    * Enable Kafka Streams support. Default: true.
     */
    Boolean enableStreams = true
 
    /**
-    * Base source directory for the Confluent plugin.
+    * Base source directory for the Confluent plugin. Default: 'src/main'.
     */
    String sourceBase = 'src/main'
 
    /**
-    * Name of the Pipeline source directory that resides in the {@link #sourceBase} directory.
+    * Name of the Pipeline source directory that resides in the {@link #sourceBase} directory. Default: 'pipeline'.
     */
    String pipelineSourceName = 'pipeline'
 
    /**
-    * Full path of the Pipeline source directory. When set, this overrides the values of {@link #sourceBase} and {@link #pipelineSourceName}.
+    * Full path of the Pipeline source directory. When set, this overrides the values of {@link #sourceBase} and {@link #pipelineSourceName}. Default: null.
     */
    String pipelineSourcePath
 
    /**
-    * The name of the Pipeline build directory in the project build directory.
+    * The name of the Pipeline build directory in the project build directory. Default: 'pipeline'.
     */
    String pipelineBuildName = 'pipeline'
 
    /**
-    * The name of the Pipeline deploy directory in the project build directory.
+    * The name of the Pipeline deploy directory in the project build directory. Default: 'pipeline'.
     */
    String pipelineDeployName = 'pipeline'
 
    /**
-    * The name of the Function deploy directory in the project build directory.
+    * The name of the Function deploy directory in the project build directory. Default: 'function'.
     */
    String functionDeployName = 'function'
 
    /**
-    * If populated, the KSQL Function JAR file will be renamed to this value during the copy. This makes it easy to hand-off to downstream deployment mechanisms.
+    * If populated, the KSQL Function JAR file will be renamed to this value during the copy. This makes it easy to hand-off to downstream deployment mechanisms. Default: null.
     */
    String functionArtifactName
 
    /**
-    * The name of the Pipeline deployment 'create' script, which contains all the persistent statements that need to be executed.
+    * The name of the Pipeline deployment 'create' script, which contains all the persistent statements that need to be executed. Default: 'ksql-script.sql'.
     */
    String pipelineCreateName = 'ksql-script.sql'
 
    /**
-    * RESTful endpoint for the KSQL Server.
+    * RESTful endpoint for the KSQL Server. Default: 'http://localhost:8088'.
     */
    String pipelineEndpoint = 'http://localhost:8088'
 
    /**
-    * The pattern used for matching the pipeline deployment artifact.
+    * The pattern used for matching the pipeline deployment artifact. Default: 'pipeline'.
     */
    String pipelinePattern = 'pipeline'
 
    /**
-    * The pattern used for matching the function deployment artifact.
+    * The pattern used for matching the function deployment artifact. Default: 'function'.
     */
    String functionPattern = 'function'
 
    /**
-    * The path of the Streams configuration file. A relative path will be resolved in the project directory, while absolute paths are resolved absolutely.
+    * The path of the Streams configuration file. A relative path will be resolved in the project directory, while absolute paths are resolved absolutely. Default: 'streams.config'.
     */
    String configPath = 'streams.config'
 
    /**
-    * The environment to pass when configuring {@link #configPath}. This uses the ConfigSlurper concept of default values with environmental overloads.
+    * The environment to pass when configuring {@link #configPath}. This uses the ConfigSlurper concept of default values with environmental overloads. Default: 'development'.
     */
    String configEnv = 'development'
 
