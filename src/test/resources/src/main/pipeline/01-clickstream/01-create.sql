604e6960e23e3c02f2ee7ef0952c1159f2fb2429
Jeffrey Lutz <jeff.lutz@agileanswers.com>
2020-06-20 21:20:22 -0400

    Added ability to switch offset between earliest and latest throughout the sql scripts

--- src/test/resources/src/main/pipeline/01-clickstream/01-create.sql
+++ src/test/resources/src/main/pipeline/01-clickstream/01-create.sql
@@ -1,3 +1,4 @@
+SET 'auto.offset.reset'='earliest';
 --SOURCE of ClickStream
 CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varchar, status int, userid int, bytes bigint, agent varchar) 
 with (kafka_topic = 'clickstream', value_format = 'json');
