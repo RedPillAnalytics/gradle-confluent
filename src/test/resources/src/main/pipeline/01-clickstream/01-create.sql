0cd6ad363faa1ed2494546c7aa267a36a5fa1895
Jeffrey Lutz <jeff.lutz@agileanswers.com>
2020-06-21 11:48:00 -0400

    Updated the pipelineexecutetask to handle SET and UNSET commands.  Also, updated the pipeline SQL examples to be proper for KSQLDB / Confluent 5.5.0

--- src/test/resources/src/main/pipeline/01-clickstream/01-create.sql
+++ src/test/resources/src/main/pipeline/01-clickstream/01-create.sql
@@ -1,10 +1,8 @@
-SET 'auto.offset.reset'='earliest';
+--SET 'auto.offset.reset'='earliest';
 --SOURCE of ClickStream
-CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varchar, status int, userid int, bytes bigint, agent varchar) 
+CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varchar, status int, userid int, bytes bigint, agent varchar)
 with (kafka_topic = 'clickstream', value_format = 'json');
 
 --BUILD STATUS_CODES
--- it's fne to have multiple comment lines
--- everybody's doing it
-CREATE TABLE clickstream_codes (code int, definition varchar) 
-with (key='code', kafka_topic = 'clickstream_codes', value_format = 'json');
+CREATE TABLE clickstream_codes (ROWKEY int key, code int, definition varchar)
+with (key = 'code', kafka_topic = 'clickstream_codes', value_format = 'json');
--- src/test/resources/src/main/pipeline/02-clickstream-users/02-integrate.sql
+++ src/test/resources/src/main/pipeline/02-clickstream-users/02-integrate.sql
@@ -1,28 +1,26 @@
-
-
 -- Use 'HAVING' Filter to show ERROR codes > 400 where count > 5
-CREATE TABLE ERRORS_PER_MIN_ALERT AS 
-SELECT status, count(*) AS errors 
-FROM clickstream window HOPPING ( size 30 second, advance by 20 second) 
-WHERE status > 400 
-GROUP BY status HAVING count(*) > 5 
+CREATE TABLE ERRORS_PER_MIN_ALERT AS
+SELECT status, count(*) AS errors
+FROM clickstream window HOPPING ( size 30 second, advance by 20 second)
+WHERE status > 400
+GROUP BY status HAVING count(*) > 5
 AND count(*) is not NULL;
 
-CREATE table ERRORS_PER_MIN AS 
-SELECT status, count(*) AS errors 
-FROM clickstream window HOPPING ( size 60 second, advance by 5  second) 
+CREATE table ERRORS_PER_MIN AS
+SELECT status, count(*) AS errors
+FROM clickstream window HOPPING ( size 60 second, advance by 5  second)
 WHERE status > 400 GROUP BY status;
 
 -- Clickstream enriched with user account data
-CREATE STREAM customer_clickstream WITH (PARTITIONS=2) AS 
-SELECT userid, u.first_name, u.last_name, u.level, time, ip, request, status, agent 
-FROM clickstream c 
-LEFT JOIN web_users u 
+CREATE STREAM customer_clickstream WITH (PARTITIONS=2) AS
+SELECT userid, u.first_name, u.last_name, u.level, time, ip, request, status, agent
+FROM clickstream c
+LEFT JOIN web_users u
 ON c.userid = u.user_id;
 
 -- View IP, username and City Versus web-site-activity (hits)
-CREATE STREAM USER_CLICKSTREAM AS 
-SELECT userid, u.username, ip, u.city, request, status, bytes 
-FROM clickstream c 
-LEFT JOIN web_users u 
+CREATE STREAM USER_CLICKSTREAM AS
+SELECT userid, u.username, ip, u.city, request, status, bytes
+FROM clickstream c
+LEFT JOIN web_users u
 ON c.userid = u.user_id;
