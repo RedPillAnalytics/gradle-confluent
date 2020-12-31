cc4db5062bbe001e766c6a7c4fe69bc0a83ce260
Stewart Bryson <stewart@redpillanalytics.com>
2020-12-31 11:44:15 -0500

    Testing against ksqldb 13 successful.

--- src/test/resources/src/main/pipeline/03-test-earliest/01-create.sql
+++ src/test/resources/src/main/pipeline/03-test-earliest/01-create.sql
@@ -1,14 +1,14 @@
 SET 'auto.offset.reset'='earliest';
 
-CREATE TABLE streama (id BIGINT, userid varchar, name varchar)
+CREATE TABLE streama (id BIGINT primary key, userid varchar, name varchar)
 with (kafka_topic = 'streama', value_format = 'json', PARTITIONS=1, REPLICAS=1);
 
 SET 'auto.offset.reset'='latest';
 
-CREATE TABLE streamb (id BIGINT, userid varchar, name varchar)
+CREATE TABLE streamb (id BIGINT primary key, userid varchar, name varchar)
 with (kafka_topic = 'streamb', value_format = 'json', PARTITIONS=1, REPLICAS=1);
 
 UNSET 'auto.offset.reset';
 
-CREATE TABLE streamc (id BIGINT, userid varchar, name varchar)
+CREATE TABLE streamc (id BIGINT primary key, userid varchar, name varchar)
 with (kafka_topic = 'streamc', value_format = 'json', PARTITIONS=1, REPLICAS=1);
